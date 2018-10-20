4a3124721ee1878465b4880d5d3ca9aa5ab364bf
Hojunwei <455623044@qq.com>
2018-10-20 21:08:36 +0800

    redis延时队列实现

--- src/main/java/com/hjw/redisdemo/app/RedisDelayingQueue.java
+++ src/main/java/com/hjw/redisdemo/app/RedisDelayingQueue.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-import java.io.Serializable;
 import java.lang.reflect.Type;
 import java.util.Set;
 import java.util.UUID;
